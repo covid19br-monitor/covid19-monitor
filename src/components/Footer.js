590cfcf9e80a810abd33d7a316bd5b0781572060
Glauber Ramos <glauberamos@gmail.com>
2020-03-22 17:47:58 -0300

    Change footer and add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,40 @@
 import React from "react";
 
 export default function Footer() {
-  var today = new Date();
-  var year = today.getFullYear();
   return (
     <footer>
+      Criado por{" "}
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.twitter.com/_juanmnl"
       >
-        &copy;{year} - juanmnl
+        @juanmnl
       </a>
-      <br />
+      {", "}
       <a
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.twitter.com/glauberamos"
       >
-        &copy;{year} - glauberamos
+        @glauberamos
+      </a>
+      {" and "}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.twitter.com/jcalmeidajr"
+      >
+        @jcalmeidajr
+      </a>
+      <br />
+      {"Colabore com o projeto no "}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/glauberramos/covid19-monitor"
+      >
+        Github
       </a>
     </footer>
   );
