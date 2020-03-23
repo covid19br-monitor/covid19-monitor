7318a47ac0b26f8d582bbb9871a64216aa4c25e5
Glauber Ramos <glauberamos@gmail.com>
2020-03-23 00:41:00 -0300

    Remove compromised

--- src/db/BrazilData.js
+++ src/db/BrazilData.js
@@ -4,56 +4,49 @@ const data = [
     name: "Rio Grande do Sul",
     capital: "Porto Alegre",
     confirmed: 71,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 1,
     name: "Acre",
     capital: "Rio Branco",
     confirmed: 11,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 2,
     name: "Amapá",
     capital: "Macapá",
     confirmed: 1,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 3,
     name: "Amazonas",
     capital: "Manaus",
     confirmed: 26,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 4,
     name: "Bahia",
     capital: "Salvador",
     confirmed: 49,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 5,
     name: "Paraná",
     capital: "Curitiba",
     confirmed: 43,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 6,
     name: "Ceará",
     capital: "Fortaleza",
     confirmed: 84,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 7,
@@ -61,7 +54,6 @@ const data = [
     capital: "Brasília",
     confirmed: 112,
     deaths: 0,
-    compromised: 0,
     status: "alert"
   },
   {
@@ -69,80 +61,70 @@ const data = [
     name: "Espírito Santo",
     capital: "Vitória",
     confirmed: 26,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 9,
     name: "Goiás",
     capital: "Goiânia",
     confirmed: 18,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 10,
     name: "Paraíba",
     capital: "João Pessoa",
     confirmed: 1,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 11,
     name: "Minas Gerais",
     capital: "Belo Horizonte",
     confirmed: 83,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 12,
     name: "Maranhão",
     capital: "São Luís",
     confirmed: 2,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 13,
     name: "Mato Grosso",
     capital: "Cuiabá",
     confirmed: 2,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 14,
     name: "Mato Grosso do Sul",
     capital: "Campo Grande",
     confirmed: 16,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 15,
     name: "Pará",
     capital: "Belém",
     confirmed: 4,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 16,
     name: "Pernambuco",
     capital: "Recife",
     confirmed: 33,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 17,
     name: "Piauí",
     capital: "Teresina",
     confirmed: 4,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 18,
@@ -150,7 +132,6 @@ const data = [
     capital: "Rio de Janeiro",
     confirmed: 119,
     deaths: 3,
-    compromised: 0,
     status: "alert"
   },
   {
@@ -158,32 +139,28 @@ const data = [
     name: "Rio Grande do Norte",
     capital: "Natal",
     confirmed: 9,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 20,
     name: "Rondônia",
     capital: "Porto Velho",
     confirmed: 3,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 21,
     name: "Roraima",
     capital: "Boa Vista",
     confirmed: 2,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 22,
     name: "Santa Catarina",
     capital: "Florianópolis",
     confirmed: 51,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 23,
@@ -191,7 +168,6 @@ const data = [
     capital: "São Paulo",
     confirmed: 459,
     deaths: 15,
-    compromised: 0,
     status: "alert"
   },
   {
@@ -199,24 +175,21 @@ const data = [
     name: "Tocantins",
     capital: "Palmas",
     confirmed: 2,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 25,
     name: "Sergipe",
     capital: "Aracaju",
     confirmed: 10,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   },
   {
     id: 26,
     name: "Alagoas",
     capital: "Maceió",
     confirmed: 7,
-    deaths: 0,
-    compromised: 0
+    deaths: 0
   }
 ];
 
